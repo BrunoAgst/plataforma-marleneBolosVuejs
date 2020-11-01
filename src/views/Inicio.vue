0ba809aee43687046db11f0920a68a022c03f249
BrunoAgst <bruno_agst@hotmail.com>
2020-11-01 19:11:50 -0300

    inserindo filtro na tela de inicio

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,16 +1,21 @@
 <template>
     <div>
         <Navbar/>
+        <div>
+            <select id="mes" v-model="mesF">
+                <option disabled value="">Escolha um mês</option>
+                <option v-for="mes in mesesF" :key="mes.id">{{mes.nome}}</option>
+            </select>
+            <a id="btFiltro" @click="valorMes()">Filtrar</a>
+        </div>
         <h1>{{mes}}</h1>
-        <p>Faturamento Atual: R$ {{valor}}</p>
+        <p>Faturamento: R$ {{valor}}</p>
     </div>
 </template>
 
 <script>
 import Navbar from '../components/Nav';
 import axios from 'axios';
-
-
 
 export default { 
     created(){
@@ -28,20 +33,128 @@ export default {
             }).catch(err => {
                 console.log(err);
         });
+
+        this.mesesF = [
+            {
+                id: 0,
+                nome: 'Janeiro'
+            },
+            {
+                id: 1,
+                nome: 'Fevereiro'
+            },
+            {
+                id: 2,
+                nome: 'Março'
+            },
+            {
+                id: 3,
+                nome: 'Abril'
+            },
+            {
+                id: 4,
+                nome: 'Maio'
+            },
+            {
+                id: 5,
+                nome: 'Junho'
+            },
+            {
+                id: 6,
+                nome: 'Julho'
+            },
+            {
+                id: 7,
+                nome: 'Agosto'
+            },
+            {
+                id: 8,
+                nome: 'Setembro'
+            },
+            {
+                id: 9,
+                nome: 'Outubro'
+            },
+            {
+                id: 10,
+                nome: 'Novembro'
+            },
+            {
+                id: 11,
+                nome: 'Dezembro'
+            }
+        ]
     },
     data(){
         return{
             dados: [],
+            mesesF: [],
             valor: 0,
             data: new Date,
             mes: '',
-            url: process.env.VUE_APP_API_URL
+            mesF: '',
+            url: process.env.VUE_APP_API_URL,
         }
     },
     components: {
         Navbar
     },
     methods:{
+        valorMes(){
+            var mes = this.mesF;
+            var numero = -1;
+            var valor = 0;
+            var data = new Date;
+
+            switch(mes){
+                case 'Janeiro':
+                    numero = 0;
+                    break;
+                case 'Fevereiro':
+                    numero = 1;
+                    break;
+                case 'Março':
+                    numero = 2;
+                    break;
+                case 'Abril':
+                    numero = 3;
+                    break;
+                case 'Maio':
+                    numero = 4;
+                    break;
+                case 'Junho':
+                    numero = 5;
+                    break;
+                case 'Julho':
+                    numero = 6;
+                    break;
+                case 'Agosto': 
+                    numero = 7;
+                    break;
+                case 'Setembro':
+                    numero = 8;
+                    break;
+                case 'Outubro':
+                    numero = 9;
+                    break;
+                case 'Novembro':
+                    numero = 10;
+                    break;
+                default: 
+                    numero = 11;
+            }
+        
+            axios.get(`${this.url}/v1/pedidos-mensal/${numero}/${data.getFullYear()}`).then(res => {
+                for(var i = 0; i < res.data.length; i++){
+                    valor += res.data[i]
+                }
+                this.mes = mes;
+                this.valor = valor;
+                
+            }).catch(err => {
+                console.log(err);
+        });
+        },
         valorTotal(array){
             for(var i = 0; i < array.length; i++){
                 this.valor += array[i]
@@ -93,5 +206,17 @@ export default {
 </script>
 
 <style scoped>
-    
+    #btFiltro{
+        cursor: pointer;;
+        border: 1px solid black;
+        padding: 3px;
+        margin-left: 5px;
+        background-color:lightseagreen;
+        color: white;
+        border-radius: 4px;
+    }
+
+    #mes{
+        height: 23px
+    }
 </style>
